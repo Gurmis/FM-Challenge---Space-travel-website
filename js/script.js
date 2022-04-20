530732b0b7c5c362c80777caf2f0ab6c77942fa5
gurmooo <gurmooo@gamil.com>
2022-04-20 12:03:50 +0200

    responsive version finished(parallax stopped yet)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,54 @@
 String.prototype.capitalize = function() { return this.charAt(0).toUpperCase() + this.slice(1);};
 (function($) {
 
+    // //nastavenie spravnej velkosti pozadia pri nacitani domovskej stranky
+    // var bgWidth = window.matchMedia('(min-width: 601px) and (max-width: 768px)').matches? '100%' : 'auto',
+    //     heightVhDifference = ($('html').height()-window.innerHeight)*0.4,
+    //     bgHeight = $('html').height()>window.innerHeight? 'calc(100% + ' + heightVhDifference.toString() + 'px)' : 'calc(100%)'
+
+    // $('html').css('backgroundSize', bgWidth + ' ' + bgHeight);
+
+    // //nastavenie spravnej sirky pozadia pri zmene velkosti okna
+    // $(window).on('resize', function(){
+    //     var bgWidth = window.matchMedia('(min-width: 601px) and (max-width: 768px)').matches? '100%' : 'auto';
+        
+    //     $('html').css('backgroundSize', bgWidth + ' ' + bgHeight);
+    // });
+
+    //breakpoints
+    var breakpoint;
+    function checkScreen(){
+        if (window.matchMedia('(min-width: 1051px)').matches) {breakpoint = 'extra large'}
+        else if((window.matchMedia('(min-width: 769px)')).matches) {breakpoint = 'desktop'}
+        else if((window.matchMedia('(min-width: 601px)')).matches) {breakpoint = 'tablet'}
+        else if((window.matchMedia('(min-width: 451px)')).matches) {breakpoint = 'mobile'}
+        else if((window.matchMedia('(max-width: 450px)')).matches) {breakpoint = 'extra small'};
+        // console.log(breakpoint)
+    }
+    checkScreen();
+    $(window).on('resize', function(){
+        checkScreen();
+        // console.log(breakpoint)
+    });
+
+    var bgBreakpoint;
+    function checkScreen(){
+        if (window.matchMedia('(min-width: 769px)').matches) {bgBreakpoint = 'desktop'}
+        else if((window.matchMedia('(max-width: 768px)')).matches) {bgBreakpoint = 'tablet'};
+        // console.log(bgBreakpoint)
+    }
+    checkScreen();
+    $(window).on('resize', function(){
+        checkScreen();
+        // console.log(bgBreakpoint)
+    });
+
+
+
+
+
+
+
         //fetch html data
         var newDataCall =$.ajax({
             url: 'sections.html',
@@ -14,9 +62,19 @@ String.prototype.capitalize = function() { return this.charAt(0).toUpperCase() +
             type: 'GET',
             dataType: 'json'
             
-        }).promise();
+        }).promise().done(function(data){
+            var destinationsList = data.destination
+            // console.log(destinationsList)
+                    destinationsList.forEach(function(item, index) {
+                        // console.log("'" + 'url(' + item.images.webp + ")'" + index)
+                        $('#preloader' + (index+1)).css({background: 'url(' + item.images.webp + ')'});
+                        // console.log('$(' + '"' + '#preloader' + (index + 1) + '"' + ')')
+            
+                      });
 
+        });
 
+       
 
         //prva sekcia na kliknutie velkym buttonom
         $('body').on('click', '.btn-main', function(event){
@@ -29,53 +87,93 @@ String.prototype.capitalize = function() { return this.charAt(0).toUpperCase() +
                 .parent().siblings().children().removeClass('selected');
 
             $('body').fadeOut('fast', function(){
-                $(this).css('background-image', 'url(./assets/destination/background-destination-desktop.jpg)');
+                $('html').css('background-image', 'url(./assets/destination/background-destination-' + bgBreakpoint + '.jpg)');
                 
                 newDataCall.then(function(data){
                     var newData = $(data).find('main' + '.' + mainClass);                                        
 
+                    $('.container').removeClass()
+                                   .addClass('container ' + mainClass);
                     $('main').html(newData.html())
                                     .attr({class: mainClass});
                 });
             })
                     .fadeIn('fast');
+
+                    
         });
 
         
         //prepnutie sekcie na kliknutie hlavnej navigacie
-        $('.navigation li').on('click', 'a', function(event){
+        $('.navigation li').on('click', function(event){
             event.preventDefault();
-            var a         = $(this),
+            var li        = $(this)
+                a         = li.children('a'),
                 mainClass = a.attr('class');
             
         //oznaci sekciu na klik
         if (a.hasClass('selected')) return;
         else {
             a.addClass('selected')
-                .parent().siblings().children().removeClass('selected');
+             .parent().siblings().children().removeClass('selected');
 
 
 
         //navigacia - zmeni background na body + vlozi patricnu sekciu na klik
-        $('body').fadeOut('fast', function(){
-                                    $(this).css('background-image', 'url(./assets/' + a.text().slice(2) + '/background-' + a.text().slice(2) + '-desktop.jpg)')
-                                    newDataCall.then(function(data){
-                                        var newData = $(data).find('main' + '.' + mainClass);                                        
+        
+            $('html').css('background-image', 'url(./assets/' + a.text().slice(2) + '/background-' + a.text().slice(2) + '-' + bgBreakpoint + '.jpg)')
+            newDataCall.then(function(data){
+                var newData = $(data).find('main' + '.' + mainClass);                                        
 
-                                        if (newData.length == 1) {
-                                            $('main').html(newData.html())
-                                                     .attr({class: mainClass});
-                                        }
-                                        else {
-                                            $('main').html('');
-                                        }
-                                    });
-                                })
-                 .fadeIn('fast');
+                if (newData.length == 1) {
+                    $('.container').removeClass()
+                                    .addClass('container ' + mainClass);
+                    $('main').html(newData.html())
+                             .attr({class: mainClass});
+                        if (window.matchMedia('(min-width: 1051px)').matches) { $('.vehicle').appendTo('.content') }
+                        else { $('.vehicle').insertAfter('.title') };
+
+                        if (window.matchMedia('(min-width: 601px)').matches) {
+                            $('.person').appendTo('.content') ;
+                            $('.selector').insertAfter('.bio');
+                            }
+                        else {
+                            $('.person').insertAfter('.title');
+                            $('.selector').insertAfter('.person');
+                            };
+                }
+                else {
+                    $('main').html('');
+                }
+            });
+
+            //pri mobilnej verzii skryje menu+overlay
+            if (window.matchMedia("(max-width: 600px)").matches) {
+                mobileNavClose();
+            }
+                                
+                
+        // $('body').fadeOut('fast', function(){
+        //                             $('html').css('background-image', 'url(./assets/' + a.text().slice(2) + '/background-' + a.text().slice(2) + '-desktop.jpg)')
+        //                             newDataCall.then(function(data){
+        //                                 var newData = $(data).find('main' + '.' + mainClass);                                        
+
+        //                                 if (newData.length == 1) {
+        //                                     $('.container').removeClass()
+        //                                                    .addClass('container ' + mainClass);
+        //                                     $('main').html(newData.html())
+        //                                              .attr({class: mainClass});
+        //                                 }
+        //                                 else {
+        //                                     $('main').html('');
+        //                                 }
+        //                             });
+        //                         })
+        //          .fadeIn('fast');
                 
         };
     });
-
+    
         //sub navigacia - zmeni podsekciu na klik
         $('body').on('click', '.sub-navigation li a', function(event){
             event.preventDefault();
@@ -91,17 +189,26 @@ String.prototype.capitalize = function() { return this.charAt(0).toUpperCase() +
 
                 //zmeni podsekciu 
                 jsonCall.then(function(data){
+                    
+
+
                     $('.planet').fadeOut(50, function(){
-                        $('.planet').css({background: 'url(' + data[mainClass][aIndex].images.webp + ')'});
+                        
+                        
+                        $('.planet').css({backgroundImage: 'url(' + data[mainClass][aIndex].images.webp + ')'});
                         $('.title.pick').html(data[mainClass][aIndex].name);
                         $('.description').html(data[mainClass][aIndex].description);
                         $('.distance').html(data[mainClass][aIndex].distance);
                         $('.travel').html(data[mainClass][aIndex].travel);
                     });
+                        
                     $('.planet').fadeIn();
+                    
 
                     $('.vehicle').fadeOut(50, function(){
-                        $('.vehicle').css({backgroundImage: 'url(' + data[mainClass][aIndex].images.portrait + ')'});
+                        var imageType = window.matchMedia('(min-width: 1051px)').matches? 'portrait' : 'landscape';                      
+
+                        $('.vehicle').css({backgroundImage: 'url(' + data[mainClass][aIndex].images[imageType] + ')'});
                         $('.description').html(data[mainClass][aIndex].description);
                         $('.name').html(data[mainClass][aIndex].name);
                     });
@@ -126,20 +233,88 @@ String.prototype.capitalize = function() { return this.charAt(0).toUpperCase() +
 
                 //zmeni podsekciu 
                 jsonCall.then(function(data){
+                    
                     $('.person').fadeOut(50, function(){
-                        $('.person').css({background: 'url(' + data[mainClass][aIndex].images.webp + ') center bottom no-repeat'})
-                        $('.role').html(data[mainClass][aIndex].role)
-                        $('.name').html(data[mainClass][aIndex].name)
-                        $('.bio').html(data[mainClass][aIndex].bio)
+                        $('.person').css({backgroundImage: 'url(' + data[mainClass][aIndex].images.webp + ') '});
+                        $('.role').html(data[mainClass][aIndex].role);
+                        $('.name').html(data[mainClass][aIndex].name);
+                        $('.bio').html(data[mainClass][aIndex].bio);
                     });
                     $('.person').fadeIn();
+                    
                 });
             };
         });
 
 
     //aktivovanie parllax efektu na pozadie
-    $('body').parallax({ friction: 0.4 });
-          
+    // $('html').parallax({ friction: 0.4 });
+    
+    
+    //jquery media queries
+    var toggler = $('.toggler'),
+        overlay = $('.overlay');
+        mobileNav = $('.navigation.top');
+        
+
+    function mobileNavClose(){
+        toggler[0].checked = false;
+        $('.overlay').fadeOut();
+    }
+
+    toggler.on('click', function(){
+        if ( toggler.is(':checked')) {
+            overlay.fadeIn();
+            mobileNav.css('display', 'flex');
+        }
+        else {overlay.fadeOut()}
+    });
+
+    overlay.on('click', function(){
+        mobileNavClose();
+    })
+
+    // $(window).on('resize', function(){
+    //     if (window.matchMedia("(max-width: 600px)").matches) {          
+    //         if(mobileNav.is(':not(:visible)')) {
+    //             mobileNav.css('display', 'flex');
+    //         }
+    //     }
+    //     else if(window.matchMedia("(min-width: 600px)").matches) {
+    //         // mobileNav.css('display', 'none')
+    //     }
+    // });
+ $(window).on('resize', function(){
+    console.log(bgBreakpoint)
+    var mainClass = $('main').attr('class');  
+     
+    $('html').css('background-image', 'url(./assets/' + mainClass + '/background-' + mainClass + '-' + bgBreakpoint + '.jpg)')
+
+    if ($('.container.technology').length == 1) {
+        jsonCall.then(function(data){
+            var a         = $('.sub-navigation a.selected'),
+                aIndex    = a.parent().index(),
+                imageType = window.matchMedia('(min-width: 1051px)').matches? 'portrait' : 'landscape';                      
+
+            $('.vehicle').css({backgroundImage: 'url(' + data[mainClass][aIndex].images[imageType] + ')'});
+            if (window.matchMedia('(min-width: 1051px)').matches) { $('.vehicle').appendTo('.content') }
+            else { $('.vehicle').insertAfter('.title') }
+    });
+    }
+
+    else if ($('.container.crew').length == 1) {
+        if (window.matchMedia('(min-width: 601px)').matches) {
+            $('.person').appendTo('.content') ;
+            $('.selector').insertAfter('.bio');
+            }
+        else {
+            $('.person').insertAfter('.title');
+            $('.selector').insertAfter('.person');
+            };
+    };
+});
+
+
 })(jQuery); 
+
 
